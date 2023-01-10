7c4e395b0ad3507f50364821770f86a0b77ce0eb
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-10 15:40:50 +0100

    proper time and date for each city

---
+++ src/script.js
@@ -0,0 +1,18 @@
+let wars = document.querySelector("#warsaw");
+let warsDate = document.querySelector("#warsaw .date");
+let warsTime = document.querySelector("#warsaw .time");
+warsDate.innerHTML = moment().tz("Europe/Warsaw").format("MMMM Do YYYY");
+warsTime.innerHTML =  moment().tz("Europe/Warsaw").format("hh:mm A");
+
+let manch = document.querySelector("#manchester");
+let manchDate = document.querySelector("#manchester .date");
+let manchTime = document.querySelector("#manchester .time");
+manchDate.innerHTML = moment().tz("Europe/London").format("MMMM Do YYYY");
+manchTime.innerHTML =  moment().tz("Europe/London").format("hh:mm A");
+
+
+let melb = document.querySelector("#melbourne");
+let melbDate = melb.querySelector(".date");
+let melbTime = document.querySelector("#melbourne .time");
+melbDate.innerHTML = moment().tz("Australia/Melbourne").format("MMMM Do YYYY");
+melbTime.innerHTML =  moment().tz("Australia/Melbourne").format("hh:mm A");
