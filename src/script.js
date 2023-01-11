260c896a5873a0b79d306a44a8ad1863c2e70eeb
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-11 12:35:42 +0100

    new cities in selector, each time zone working properly

--- src/script.js
+++ src/script.js
@@ -18,3 +18,19 @@ let melbTime = document.querySelector("#melbourne .time");
 melbDate.innerHTML = moment().tz("Australia/Melbourne").format("MMMM Do YYYY");
 melbTime.innerHTML =  moment().tz("Australia/Melbourne").format("hh:mm:ss [<small>]A[</small>]");
 }, 1000);
+
+function update(event){
+   let cityTZ = event.target.value; 
+   let cityNames = cityTZ.replace("_", " ").split("/")[1]; 
+   let cityT = moment().tz(cityTZ);
+   let cityElement = document.querySelector(".city");
+   cityElement.innerHTML = `<div class="cities">
+                <h2>${cityNames}</h2>
+                <div class="date">${cityT.format("MMMM Do YYYY")}</div>
+                <div class="time">${cityT.format("h:mm:ss")} <small>${cityT.format("A")}</div>
+            </div>`
+}
+
+
+let citySelect = document.querySelector("#selector");
+citySelect.addEventListener("change", update);
